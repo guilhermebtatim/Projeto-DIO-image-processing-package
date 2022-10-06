5752f0e9bd518052cd1f96cd84b568dd11e9f586
guilhermebtatim <113799643+guilhermebtatim@users.noreply.github.com>
2022-10-06 18:16:24 -0300

    Add files via upload
    Projeto renomeado para upload no Test Pypi.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing",
+    name="Projeto DIO image_processing",
     version="0.0.1",
     author="Guilherme",
     description="Image Processing Package using Skimage",
